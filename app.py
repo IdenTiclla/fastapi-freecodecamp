7390c5f8e58cbc4b2434bb2f123cb3df1ae4511a
unknown <iden.ticlla@gmail.com>
2021-08-13 08:03:15 -0400

    Query Parameters

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, Path
+from typing import Optional
 
 app = FastAPI()
 students = {
@@ -6,7 +7,12 @@ students = {
         'name': 'john',
         'age': 14,
         'class': 'year 12'
-    }
+    },
+    2: {
+        'name': 'tim',
+        'age': 11,
+        'class': 'year 11'
+    },
 }
 @app.get('/')
 def index():
@@ -16,3 +22,13 @@ def index():
 @app.get('/get-student/{student_id}')
 def get_student(student_id: int = Path(None, description='The ID of the student you want to view', gt=0, lt=3)):
     return students[student_id]
+
+# get-by-name?name='alex'
+@app.get('/get-by-name')
+def get_student_by_name(*, name: Optional[str] = None, test : int):
+    for student_id in students:
+        if students[student_id]['name'] == name:
+            return students[student_id]
+        
+    return {"data": "Not Found."}
+
